0dc89623037b8090522c1fb3a0a172df7955499c
bbb <bbb>
2024-04-22 16:17:38 +0200

    .

--- examples/image-embeddings-1.py
+++ examples/image-embeddings-1.py
@@ -4,6 +4,8 @@ url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 print(image)
 
+# see https://github.com/minimaxir/imgbeddings
+
 from imgbeddings import imgbeddings
 ibed = imgbeddings()
 embedding = ibed.to_embeddings(image)
